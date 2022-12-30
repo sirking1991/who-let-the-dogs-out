3f9892eea8a004f6bdd4a96b877900a0083482cd
Sherwin <sirking1991@gmail.com>
2022-12-30 20:25:47 +0800

    implemented breeds list

--- app/Http/Livewire/Breeds.php
+++ app/Http/Livewire/Breeds.php
@@ -12,27 +12,7 @@ class Breeds extends Component
 {
     public function render()
     {
-        $breeds = Cache::remember(
-            'breeds',
-            now()->addDay(), 
-            function(){
-                $response = Http::get(config('app.dog-ceo-api') . '/breeds/list/all');
-                $response->throw();
-
-                $breeds = [];
-                foreach ($response->json('message') as $parent => $child) {
-                    if (!empty($child)) {
-                        foreach($child as $breed) {
-                            $breeds[] = $parent . ' : ' . $breed;
-                        }
-                    } else {
-                        $breeds[] = $parent;
-                    }
-                }
-            }
-        );
-
-        dd($breeds);
+        $breeds = getBreeds();
 
         return view('livewire.breeds', ['breeds' => $breeds]);
     }
--- resources/views/livewire/breeds.blade.php
+++ resources/views/livewire/breeds.blade.php
@@ -1,11 +1,19 @@
 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 text-gray-900 dark:text-gray-100 overflow-scroll">
-            <ul>
-                @foreach ($breeds as $breed)
-                    <li>{{ $breed }}</li>
-                @endforeach
-            </ul>
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900 overflow-scroll">
+            <div class="d-flex flex-row">
+            @foreach ($breeds as $breed)
+            <div class='p-2'>
+                <div class="card" style="width: 15rem; height:300px;">
+                    <img src="{{ getBreedImage($breed) }}" style="height: 200px;" class="img-thumbnail card-img-top" alt="{{ $breed }}">
+                    <div class="card-body">
+                    <h5 class="card-title fw-bold">{{ ucfirst($breed) }}</h5>
+                    <button class="btn btn-primary">Like this</button>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+            </div>
         </div>
     </div>
 </div>
