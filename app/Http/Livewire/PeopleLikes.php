0250067428433f5ffa143931367cd78dcb0f5d06
Sherwin <sirking1991@gmail.com>
2022-12-30 22:05:28 +0800

    implemented PeopleLikes

--- app/Http/Livewire/PeopleLikes.php
+++ app/Http/Livewire/PeopleLikes.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class PeopleLikes extends Component
 {
     public function render()
     {
-        return view('livewire.people-likes');
+        $users = User::whereNot('id', Auth::user()->id)->get();
+
+        return view('livewire.people-likes', ['users' => $users]);
     }
 }
--- resources/views/livewire/people-likes.blade.php
+++ resources/views/livewire/people-likes.blade.php
@@ -1,7 +1,27 @@
 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 text-gray-900">
-            This list shows what other people likes
+        <div class="p-6 text-gray-900 overflow-scroll">
+            <h3>Breeds others like</h3>
+
+            <div class="d-flex flex-row">
+                @foreach ($users as $user)
+                <div class='p-2'>
+                    <div class="card" style="width: 15rem; height:300px;">
+                        <img src="{{ Gravatar::get($user->email) }}" style="height: 160px;" class="img-thumbnail card-img-top" alt="{{ $user->name }}">
+                        <div class="card-body">
+                        <h5 class="card-title fw-bold">{{ ucfirst($user->name) }} likes</h5>
+                        <ul>
+                            @foreach ($user->likes as $like)
+                            <li>                                
+                                {{ ucfirst($like->breed) }}
+                            </li>
+                            @endforeach
+                        </ul>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>            
         </div>
     </div>
 </div>
